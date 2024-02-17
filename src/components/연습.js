956181a8258052f47a3123e4feb6dd4cc8999f3d
juwonkang <wndnjs1100@gmail.com>
2024-02-17 19:03:25 +0900

    연습

--- src/components/연습.js
+++ src/components/연습.js
@@ -1,8 +1,26 @@
-let map = new Map()
-map.set('숫자', 1)
-map.set('문자형', 'what')
-map.set(true, 'true')
+//---------------------map()-------------------------
 
-console.log(map.get('숫자'))
-console.log(map.get('문자형'))
-console.log(map.get(true))
+// const strings = ['1', '2', '3', '4']
+
+// const change = (string) => Number(string)
+
+// const stringNumber1 = strings.map((string) => Number(string))
+
+// console.log(stringNumber1)
+// console.log(strings[0])
+// console.log(typeof strings[0])
+// console.log(stringNumber1[0])
+// console.log(typeof stringNumber1[0])
+
+// console.log(strings !== stringNumber1)
+
+//---------------------map()-------------------------
+
+//---------------------filter()-------------------------
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const evennumbers1 = numbers.filter((num) => num % 2 === 0)
+console.log(evennumbers1)
+
+//---------------------filter()-------------------------
